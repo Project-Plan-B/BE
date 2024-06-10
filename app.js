8d722356bc0e064b95ad9794e354eae2fe30d43b
Yu Jun Choi <dmddkslek0@gmail.com>
2024-06-10 17:29:24 +0900

    ⚡️

--- app.js
+++ app.js
@@ -1,17 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
+const WebSocket = require('ws');
+
 const app = express();
 const port = 3000;
 
-const sensorRoutes = require('./routes/sensor');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-// 미들웨어 설정
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// 라우트 설정
-app.use('/sensor-data', sensorRoutes);
+let clients = [];
 
-app.listen(port, () => {
+wss.on('connection', (ws) => {
+    clients.push(ws);
+    ws.on('close', () => {
+    clients = clients.filter(client => client !== ws);
+    });
+});
+
+app.post('/sensor-data', (req, res) => {
+    const { temperature, humidity, co2, ir1, ir2, ir3, ir4 } = req.body;
+
+    console.log(`Temperature: ${temperature}`);
+    console.log(`Humidity: ${humidity}`);
+    console.log(`CO2: ${co2}`);
+    console.log(`IR Sensor 1: ${ir1}`);
+    console.log(`IR Sensor 2: ${ir2}`);
+    console.log(`IR Sensor 3: ${ir3}`);
+    console.log(`IR Sensor 4: ${ir4}`);
+
+    const sensorData = {
+    temperature,
+    humidity,
+    co2,
+    ir1,
+    ir2,
+    ir3,
+    ir4
+    };
+
+  // 모든 연결된 클라이언트에게 데이터 전송
+    lients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(sensorData));
+    }
+    });
+
+    res.send('Data received');
+});
+
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- routes/sensor.js
+++ routes/sensor.js
@@ -2,19 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    const { temperature, humidity, co2, ir1, ir2, ir3, ir4 } = req.body;
+  const { temperature, humidity, co2, ir1, ir2, ir3, ir4 } = req.body;
 
-    console.log(`Temperature: ${temperature}`);
-    console.log(`Humidity: ${humidity}`);
-    console.log(`CO2: ${co2}`);
-    console.log(`IR Sensor 1: ${ir1}`);
-    console.log(`IR Sensor 2: ${ir2}`);
-    console.log(`IR Sensor 3: ${ir3}`);
-    console.log(`IR Sensor 4: ${ir4}`);
+  console.log(`Temperature: ${temperature}`);
+  console.log(`Humidity: ${humidity}`);
+  console.log(`CO2: ${co2}`);
+  console.log(`IR Sensor 1: ${ir1}`);
+  console.log(`IR Sensor 2: ${ir2}`);
+  console.log(`IR Sensor 3: ${ir3}`);
+  console.log(`IR Sensor 4: ${ir4}`);
 
-  // 데이터를 데이터베이스에 저장하거나 다른 처리를 여기에 추가
+  const sensorData = {
+    temperature,
+    humidity,
+    co2,
+    ir1,
+    ir2,
+    ir3,
+    ir4
+  };
 
-    res.send('Data received');
+  // 모든 연결된 클라이언트에게 데이터 전송
+  clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(sensorData));
+    }
+  });
+
+  res.send('Data received');
 });
 
 module.exports = router;
